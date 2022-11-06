f9a105623a7ea0f98959e41cc87ebe4c06cc0cdb
agoodman1999 <agoodman@civiconnect.ca>
2022-11-06 09:06:09 -0500

    Clean up code in injector.js

--- server/injector.js
+++ server/injector.js
@@ -1,30 +1,35 @@
 import strapiRequest from "./util/strapiRequest";
 
-const collectionEls = document.body.querySelectorAll("[strapi-collection]");
+//find all the elements with the strapi-collection attribute
+const collectionElms = document.body.querySelectorAll("[strapi-collection]");
 
-// Loop through all elements with the strapi-collection attribute
-collectionEls.forEach(async (el) => {
-  // Get the collection name from the attribute
-  const collectionName = el.getAttribute("strapi-collection");
-  const strapiData = await strapiRequest(collectionName);
-  console.log(strapiData);
-  //NOTE - if childNodes[0] is not our template element, it will cause a bug
-  const templateEl = el.children[0].cloneNode(true);
-  el.children[0].remove();
-  // for loop that iterates through the mock data
-    for (let i = 0; i < strapiData.length; i++) {
-        const { id, attributes } = strapiData[i];
-        const clone = templateEl.cloneNode(true);
-        el.appendChild(clone);
-        const fieldEls = clone.querySelectorAll("[field-id]");
-        // Loop through all the field elements
-        fieldEls.forEach((fieldEl) => {
-            // Get the field id from the attribute
-            const fieldId = fieldEl.getAttribute("field-id");
-            // Get the field value from the mock data
-            const fieldValue = attributes[fieldId];
-            // Set the field value
-            fieldEl.innerHTML = fieldValue;
-        });
-    }
-});
+//for each collection element 
+collectionElms.forEach(async (collectionElm) => {
+	//clone the template item elm and remove it from the dom
+	const itemTemplateElm = collectionElm.children[0].cloneNode(true)
+	collectionElm.children[0].remove()
+
+	//get the collection item data from strapi
+	const collectionData = await strapiRequest(collectionElm.getAttribute("strapi-collection"))
+
+	//loop through the collection data and add it to a new clone of the template item elm
+	for (let i = 0; i < collectionData.length; i++) {
+		const { id, attributes } = collectionData[i];
+		const itemElm = itemTemplateElm.cloneNode(true);
+
+		//find all the field elements in the item elm
+		const fieldElms = itemElm.querySelectorAll("[field-id]");
+
+		//loop through the field elements and set the inner html to the field value from the collection data
+		fieldElms.forEach((fieldElm) => {
+			const fieldId = fieldElm.getAttribute("field-id");
+			const fieldValue = attributes[fieldId];
+			fieldElm.innerHTML = fieldValue;
+		});
+
+		//add the item elm to the collection elm
+		collectionElm.appendChild(itemElm);
+	}
+})
+
+
