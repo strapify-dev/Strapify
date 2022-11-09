2cb997fd6f2e643f5b71bf2d7704c5f13dcfc675
agoodman1999 <agoodman@civiconnect.ca>
2022-11-09 10:32:23 -0500

    change webpack to bundle injector.js into bundle folder.

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	entry: ['./injector.js'],
 	output: {
-		path: path.resolve(__dirname, 'output'),
+		path: path.resolve(__dirname, 'bundle'),
 		filename: 'main.js'
 	},
 	//mode: 'production',
