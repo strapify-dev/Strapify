d2606986a9d58eaa8afaa5a5122f0038e2eed435
agoodman1999 <agoodman@civiconnect.ca>
2022-11-16 13:07:44 -0500

    fix trailing slash bug on strapi url.

--- server/injector.js
+++ server/injector.js
@@ -5,6 +5,8 @@ const this_script = document.currentScript;
 let strapi_api_url;
 if (this_script.hasAttribute("data-strapi-api-url")) {
 	strapi_api_url = this_script.attributes.getNamedItem("data-strapi-api-url").value;
+	//remove trailing slash
+	strapi_api_url = strapi_api_url.replace(/\/$/, "");
 } else {
 	strapi_api_url = "http://localhost:1337";
 }
