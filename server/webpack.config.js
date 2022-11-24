9d14778b9e838b02ea07fd7c72ccf20d43b4490a
agoodman1999 <agoodman@civiconnect.ca>
2022-11-24 11:46:41 -0500

    update package.json to use package name strapify and add production build script. Update webpack config to output a file with a different name when bundling for production.

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
 
-module.exports = {
-	entry: [path.resolve(__dirname, './strapify-src/injector.js')],
-	output: {
-		path: path.resolve(__dirname, './bundle'),
-		filename: 'main.js'
-	},
-	//mode: 'production',
-	mode: 'development',
+module.exports = (env, argv) => {
+	return {
+		entry: [path.resolve(__dirname, './strapify-src/injector.js')],
+		output: {
+			path: path.resolve(__dirname, './bundle'),
+			filename: argv.mode === "development" ? 'main.js' : "strapify.js"
+		},
+		//mode: 'production',
+		//mode: 'development',
 
-	module: {
+		module: {
 
+		}
 	}
 }
