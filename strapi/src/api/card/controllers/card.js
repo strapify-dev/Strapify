f6edac092de31032090230650f20c0b64f303338
agoodman1999 <agoodman@civiconnect.ca>
2022-12-06 09:30:53 -0500

    delete strapi

--- strapi/src/api/card/controllers/card.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * card controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::card.card');
--- strapi/src/api/card/services/card.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * card service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::card.card');
