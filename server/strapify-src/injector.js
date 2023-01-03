12a6e4629feefe976e7a7e4156ca4910ae41c21a
agoodman1999 <agoodman@civiconnect.ca>
2023-01-03 11:27:37 -0500

    code cleanup.

--- server/strapify-src/injector.js
+++ server/strapify-src/injector.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	//if the user exists, make a request to the user endpoint to test the jwt
 	if (user) {
 		strapiRequest("/api/users/me").then((response) => {
-			//dispatch a custom event with the user data
+			//when we succeed, dispatch a custom event with the user data
 			document.dispatchEvent(new CustomEvent("strapiUserAuthenticated", {
 				bubbles: false,
 				detail: {
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 				}
 			}));
 		}).catch((error) => {
-			//dispatch a custom event to indicate that the user authentication failed
+			//when we fail, dispatch a custom event to indicate that the user authentication failed
 			document.dispatchEvent(new CustomEvent("strapiUserAuthenticationError", {
 				bubbles: false,
 				detail: {
@@ -31,71 +31,71 @@ document.addEventListener("DOMContentLoaded", () => {
 				}
 			}));
 
-			//remove the user and jwt from local storage
+			//then remove the user and jwt from local storage
 			localStorage.removeItem("user");
 			localStorage.removeItem("jwt");
 
-			//refresh the page
+			//then refresh the page
 			window.location.reload();
 		}).finally(() => {
+			//in any case, strapify!!!
 			strapify();
 		});
 	} else {
+		//if there was no user in local storage, strapify!!!
 		strapify();
 	}
 });
 
+//when the strapify has initialized, write a message to the console
 document.addEventListener("strapifyInitialized", () => {
 	console.log("strapify finished");
 });
 
+//this is essentially the entry point for Strapify. It is called when the DOM is ready and user authenticated has been handled
 async function strapify() {
 	//create a class called strapify-hide and insert it into the head
 	const strapifyHideStyle = document.createElement("style");
 	strapifyHideStyle.innerHTML = ".strapify-hide { display: none !important; }";
 	document.head.appendChild(strapifyHideStyle);
 
-	//find all elements with the strapi-delete attribute
+	//find all elements with the strapi-delete attribute and remove them
 	const deleteElms = document.body.querySelectorAll("[strapi-delete]");
-
-	//remove all delete elements
 	deleteElms.forEach(deleteElm => deleteElm.remove());
 
-	//find all elements with strapi-single-type attributes 
+	//find all top level elements (not descendents/processed by other strapify elements)
 	const singleTypeElms = document.querySelectorAll(Strapify.validStrapifySingleTypeAttributes.map((attr) => `[${attr}]`).join(", "));
-
-	//find all the elements with the strapi-collection attribute 
 	const collectionElms = document.body.querySelectorAll("[strapi-collection]");
-
-	//find all the elements with the strapi-form or strapi-auth attribute
 	const formElms = document.body.querySelectorAll("[strapi-form], [strapi-auth]");
-
-	//find all the elements with the strapi-logout attribute
 	const logoutElms = document.body.querySelectorAll("[strapi-logout]");
-
-	//find all elements with strapi-ezforms-form attributes
 	const ezFormsElms = Strapify.findEZFormElms();
 
-	const promises = []
+	//the elements will be processed asynchronously, so we store the promises in an array on which we can await
+	const processPromises = []
 
+	//instantiate the strapify objects and process them
 	for (let i = 0; i < formElms.length; i++) {
 		const formElm = formElms[i]
 		const strapifyForm = new StrapifyForm(formElm)
-		promises.push(strapifyForm.process())
+		processPromises.push(strapifyForm.process())
 	}
-
 	for (let i = 0; i < singleTypeElms.length; i++) {
 		const singleTypeElm = singleTypeElms[i]
 		const strapifySingleType = new StrapifySingleType(singleTypeElm);
-		promises.push(strapifySingleType.process());
+		processPromises.push(strapifySingleType.process());
 	}
-
 	for (let i = 0; i < collectionElms.length; i++) {
 		const collectionElm = collectionElms[i]
 		const strapifyCollection = new StrapifyCollection(collectionElm);
-		promises.push(strapifyCollection.process());
+		processPromises.push(strapifyCollection.process());
+	}
+	for (let i = 0; i < ezFormsElms.length; i++) {
+		const ezFormsElm = ezFormsElms[i]
+		const strapifyEZFormsForm = new StrapifyEZFormsForm(ezFormsElm);
+		processPromises.push(strapifyEZFormsForm.process());
 	}
 
+	//logout elements are a simple case, so we just handle them here
 	for (let i = 0; i < logoutElms.length; i++) {
 		const logoutElm = logoutElms[i]
 		logoutElm.addEventListener("click", () => {
@@ -105,14 +105,10 @@ async function strapify() {
 		});
 	}
 
-	for (let i = 0; i < ezFormsElms.length; i++) {
-		const ezFormsElm = ezFormsElms[i]
-		const strapifyEZFormsForm = new StrapifyEZFormsForm(ezFormsElm);
-		strapifyEZFormsForm.process();
-	}
+	//wait for all the strapify objects to finish processing
+	await Promise.allSettled(processPromises)
 
-	await Promise.allSettled(promises)
-
+	//webflow cancer treatment
 	// if (window.Webflow && window.Webflow.require) {
 	// 	console.log("reinitializing ix2 (in strapify-injector)")
 	// 	window.Webflow.destroy();
@@ -121,7 +117,7 @@ async function strapify() {
 	// 	document.dispatchEvent(new Event("readystatechange"));
 	// }
 
-	//dispatch custom event with the collection data
+	//dispatch the strapifyInitialized event
 	document.dispatchEvent(new CustomEvent("strapifyInitialized", {
 		bubbles: false,
 		detail: {
