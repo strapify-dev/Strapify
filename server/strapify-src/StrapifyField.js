e2f80137aa2084d6fae2f4bf3688533ad51c6597
agoodman1999 <agoodman@civiconnect.ca>
2023-01-03 13:13:07 -0500

    code cleanup.

--- server/strapify-src/StrapifyField.js
+++ server/strapify-src/StrapifyField.js
@@ -1,12 +1,17 @@
 import Strapify from "./Strapify"
 
 class StrapifyField {
+	//the field element this class manages
 	#fieldElement;
+	//the strapi data attributes, to be passed from the template
 	#strapiDataAttributes
+	//mutation observer to watch for strapify attribute changes
 	#mutationObserver;
 
+	//the css classes that have been added to the field element (strapi-class-add, strapi-class-conditional, ...)
 	#managedClasses = [];
 
+	//the allowed strapify attributes for the field element
 	#attributes = {
 		"strapi-field": undefined,
 		"strapi-class-add": undefined,
@@ -35,16 +40,19 @@ class StrapifyField {
 		});
 	}
 
+	//soft destroy that disconnects the mutation observer
 	destroy() {
 		this.#mutationObserver.disconnect();
 	}
 
+	//update data attributes
 	#updateAttributes() {
 		Object.keys(this.#attributes).forEach((attribute) => {
 			this.#attributes[attribute] = this.#fieldElement.getAttribute(attribute);
 		})
 	}
 
+	//for strapi-field
 	#processStrapiFieldElms(strapiAttributes) {
 		let attributeValue = this.#fieldElement.getAttribute("strapi-field");
 		attributeValue = Strapify.substituteQueryStringVariables(attributeValue)
@@ -53,6 +61,7 @@ class StrapifyField {
 		Strapify.modifyElmWithStrapiData(strapiDataValue, this.#fieldElement);
 	}
 
+	//for strapi-class-add
 	#processStrapiClassAddElms(strapiAttributes) {
 		const attributeValue = this.#fieldElement.getAttribute("strapi-class-add");
 		const strapiFieldNames = attributeValue.split("|");
@@ -65,6 +74,7 @@ class StrapifyField {
 		})
 	}
 
+	//for strapi-class-replace
 	#processStrapiClassReplace(strapiAttributes) {
 		const attributeValue = this.#fieldElement.getAttribute("strapi-class-replace");
 		const args = attributeValue.split("|");
@@ -83,6 +93,7 @@ class StrapifyField {
 		})
 	}
 
+	//for strapi-class-conditional
 	#processStrapiConditionalClass(strapiDataAttributes) {
 		const attributeValue = this.#attributes["strapi-class-conditional"];
 
@@ -105,6 +116,7 @@ class StrapifyField {
 		})
 	}
 
+	//for strapi-into
 	#processStrapiInto(strapiAttributes) {
 		const attributeValue = this.#fieldElement.getAttribute("strapi-into");
 		const args = attributeValue.split("|");
@@ -114,21 +126,21 @@ class StrapifyField {
 			const intoAttributeName = split[1].trim();
 			let intoDataValue = split[0].trim();
 			console.log(intoDataValue);
-			
+
 			//strapi variables are wrapped in double curly braces
 			const regex = /{{(.*?)}}/g;
 			//get all strapi variables in arg and replace with value from getStrapiComponentValue
 			const matches = arg.match(regex);
 
-			// if no matches, then no strapi variables in arg
+			//if no matches, then no strapi variables in arg
 			if (!matches) {
 				intoDataValue = Strapify.substituteQueryStringVariables(intoDataValue);
 				intoDataValue = Strapify.getStrapiComponentValue(intoDataValue, strapiAttributes);
 				this.#fieldElement.setAttribute(intoAttributeName, intoDataValue);
 				return;
 			}
-			
-			// otherwise, replace strapi variables with values from getStrapiComponentValue
+
+			//otherwise, replace strapi variables with values from getStrapiComponentValue
 			matches.forEach((match) => {
 				const strapiFieldName = match.substring(2, match.length - 2);
 				let strapiValue = Strapify.substituteQueryStringVariables(strapiFieldName);
@@ -140,6 +152,7 @@ class StrapifyField {
 		})
 	}
 
+	//for strapi-css-rule
 	#processStrapiCSSRule(strapiAttributes) {
 		const attributeValue = this.#fieldElement.getAttribute("strapi-css-rule");
 		const args = attributeValue.split("|").map(arg => arg.trim());
@@ -169,6 +182,7 @@ class StrapifyField {
 	process(strapiDataAttributes) {
 		this.#strapiDataAttributes = strapiDataAttributes;
 
+		//restore the set of css classes on the fieldElement to the state it was in before Strapify was applied
 		this.#managedClasses.forEach((managedClass) => {
 			if (managedClass.state === "added") {
 				this.#fieldElement.classList.remove(managedClass.name);
@@ -178,29 +192,20 @@ class StrapifyField {
 		})
 		this.#managedClasses = [];
 
-		if (this.#attributes["strapi-field"] !== null && this.#attributes["strapi-field"] !== undefined) {
-			this.#processStrapiFieldElms(strapiDataAttributes);
+		//execute the appropriate function for each defined data attribute
+		const attributeToFunctionMap = {
+			"strapi-field": this.#processStrapiFieldElms,
+			"strapi-class-add": this.#processStrapiClassAddElms,
+			"strapi-class-replace": this.#processStrapiClassReplace,
+			"strapi-class-conditional": this.#processStrapiConditionalClass,
+			"strapi-into": this.#processStrapiInto,
+			"strapi-css-rule": this.#processStrapiCSSRule
 		}
-
-		if (this.#attributes["strapi-class-add"]) {
-			this.#processStrapiClassAddElms(strapiDataAttributes);
-		}
-
-		if (this.#attributes["strapi-class-replace"]) {
-			this.#processStrapiClassReplace(strapiDataAttributes);
-		}
-
-		if (this.#attributes["strapi-class-conditional"]) {
-			this.#processStrapiConditionalClass(strapiDataAttributes);
-		}
-
-		if (this.#attributes["strapi-into"]) {
-			this.#processStrapiInto(strapiDataAttributes);
-		}
-
-		if (this.#attributes["strapi-css-rule"]) {
-			this.#processStrapiCSSRule(strapiDataAttributes);
-		}
+		Object.keys(attributeToFunctionMap).forEach((attribute) => {
+			if (this.#attributes[attribute]) {
+				attributeToFunctionMap[attribute].call(this, strapiDataAttributes);
+			}
+		})
 	}
 }
 
