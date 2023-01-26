bdb7939fcfa6f5e639b5c43a2ad6bf8315f620fb
agoodman1999 <agoodman@civiconnect.ca>
2023-01-26 12:11:56 -0500

    add scripts and package for gzip compression

--- strapify/webpack.config.js
+++ strapify/webpack.config.js
@@ -1,21 +1,25 @@
 const path = require('path');
-const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
+//const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = (env, argv) => {
+	const plugins = [];
+	if (env.analyze) {
+		plugins.push(new WebpackBundleAnalyzer.BundleAnalyzerPlugin());
+	}
+	if (env.gzip) {
+		plugins.push(new CompressionPlugin({
+			algorithm: 'gzip',
+		}));
+	}
+
 	return {
 		entry: [path.resolve(__dirname, './src/injector.js')],
 		output: {
 			path: path.resolve(__dirname, './bundle'),
 			filename: argv.mode === "development" ? 'main.js' : "strapify.js"
 		},
-		//mode: 'production',
-		//mode: 'development',
 
-		module: {
-
-		},
-		plugins: [
-			new WebpackBundleAnalyzer.BundleAnalyzerPlugin()
-		]
+		plugins: plugins
 	}
 }
